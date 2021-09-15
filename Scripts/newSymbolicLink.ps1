4b2f65ff22e16aedd65d972eeba45536a30ee90d
cb <christoffer.broback@gmail.com>
2021-09-15 19:44:20 +0200

    trying to merge work with home

--- Scripts/newSymbolicLink.ps1
+++ Scripts/newSymbolicLink.ps1
@@ -1,3 +1,5 @@
-﻿
+﻿link to app launcher
+store aplauncher locally, link it's apfolder 
+New-Item -Path 'H:\mina grejer\Till Github' -ItemType SymbolicLink -Value 'H:\mina grejer\Project shelf\Till Github'
 
 New-Item -Path 'H:\mina grejer\Till Github' -ItemType SymbolicLink -Value 'H:\mina grejer\Project shelf\Till Github'
