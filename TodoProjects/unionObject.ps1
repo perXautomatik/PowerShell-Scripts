98606802931c9380d437cc6e5622fb128e315530
perXautomatik <christoffer.broback@gmail.com>
2021-12-27 16:08:30 +0100

    dupesRemoved,MovedToScripts

--- TodoProjects/unionObject.ps1
+++
@@ -1,5 +0,0 @@
-Function Union-Object ([String[]]$Property = @()) {             # https://powersnippets.com/union-object/
-    $Objects = $Input | ForEach {$_}                            # Version 02.00.01, by iRon
-    If (!$Property) {ForEach ($Object in $Objects) {$Property += $Object.PSObject.Properties | Select -Expand Name}}
-    $Objects | Select ([String[]]($Property | Select -Unique))
-} Set-Alias Union Union-Object
