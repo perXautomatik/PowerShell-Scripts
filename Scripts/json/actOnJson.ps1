32d3b57fd502f562bbd6f6c73676dd383b1eac4e
perXautomatik <christoffer.broback@gmail.com>
2021-12-27 17:01:12 +0100

    furtherReorganization

--- Scripts/json/actOnJson.ps1
+++ Scripts/json/actOnJson.ps1
@@ -1,13 +1,12 @@
-﻿$jsonx = "C:\Users\chris\OneDrive\Desktop\cluster-windows.json"
+﻿cd (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+. .\getChildrenRecursive.ps1
+. .\presentJson.ps1
 
-$json = [ordered]@{}
+$jsonx = "D:\OneDrive\TabSessionManager - Backup\b33fa6d5-141a-419a-aa4e-c62c5e204965"
+$jsonx = Get-Content $jsonx -Raw | ConvertFrom-Json
+
+$presentationMethodPath = (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) -ChildPath "presentJson.ps1" )
 
 
-(Get-Content $jsonx -Raw | ConvertFrom-Json).PSObject.Properties |
-    ForEach-Object { $json[$_.Name] = $_.Value }
-
-
-
-$json.SyncRoot
-
+getChildrenRecursive $jsonx $presentationMethodPath
 
