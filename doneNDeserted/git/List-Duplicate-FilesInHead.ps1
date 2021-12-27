a8638851c9f6a5654ff43571145fef8d8506513e
perXautomatik <christoffer.broback@gmail.com>
2021-12-27 13:43:51 +0100

    etc
    Movedarround.
    Removed zero size files
    
    # Conflicts:
    #	.gitignore
    #	Scripts
    #	Snipps/Example-Function-hello2.ps1
    #	Snipps/hello.ps1
    #	ToMerge/ClearScrean.ps1
    #	ToMerge/reg3.ps1
    #	TodoProjects/Algoritmix/crossAnalysedContainsHirarchy.md
    #	TodoProjects/MergExifData.md
    #	TodoProjects/Tokenization.ps1
    #	TodoProjects/fileSizeAnalyzis.md
    #	TodoProjects/findSimilarFolders.md
    #	TodoProjects/git/allFoldersAsSubmodules.md
    #	TodoProjects/git/git-createModuleFromGist.md
    #	TodoProjects/git/git-mapTowardsOtherStructure.ps1
    #	TodoProjects/git/git-revursivlyConsumeNestedGits.md
    #	TodoProjects/git/gitCleanup.md
    #	TodoProjects/git/gitCreateXbranches.ps1
    #	TodoProjects/git/gitRecursivlyConsumeSubmodules.ps1
    #	TodoProjects/link to app launcher.md
    #	TodoProjects/listMostCommonExtensionInapth.md
    #	TodoProjects/split-join by runconfig.ps1
    #	doneAndDeserted/Betesta/Esp-Header-Export/.gitmodules
    #	doneAndDeserted/Betesta/Esp-Header-Export/LICENSE
    #	doneAndDeserted/Betesta/Esp-Header-Export/packextension.ps1
    #	doneAndDeserted/Betesta/Esp-Header-Export/readHeaderEsp.ps1
    #	doneAndDeserted/Betesta/Esp-Header-Export/todo-git-take all extensions.ps1
    #	doneAndDeserted/Betesta/bsarch/packextension.ps1
    #	doneAndDeserted/json/Json-join-bookmarks.ps1
    #	doneAndDeserted/json/json an-Intersect.ps1
    #	doneAndDeserted/pdf/PdfSharp5.ps1
    #	doneAndDeserted/works but need better presentation/NewFeatures.ps1
    #	doneNDeserted/Textproc/ConvertExcelToCsvInBatch.ps1
    #	doneNDeserted/Textproc/CountUniqueLines.ps1
    #	doneNDeserted/Textproc/orderFileByOtherFile.ps1
    #	doneNDeserted/choco/chocExport.ps1
    #	doneNDeserted/choco/chocoInstall.ps1
    #	doneNDeserted/downloadsNapi/downloadAndExecuteExternalPs1.ps1
    #	doneNDeserted/downloadsNapi/downloadUrl.ps1
    #	doneNDeserted/downloadsNapi/lastFmRestApi.ps1
    #	doneNDeserted/filesystem/RecursiveAttemptSumUpFiles.ps1
    #	doneNDeserted/filesystem/junctionEmptyFoldersInotherTree.ps1
    #	doneNDeserted/filesystem/recurse-FileSize.ps1
    #	doneNDeserted/filesystem/rename folders in folder with prefix of most common file within.ps1
    #	doneNDeserted/filesystem/splitFile.ps1
    #	doneNDeserted/git/gitCopyDiffToMv.ps1
    #	doneNDeserted/git/gitSearchAllBranches.ps1
    #	doneNDeserted/git/install-GitIgnore.IO.ps1
    #	doneNDeserted/git/sh/gitsubmodulerebuild.sh
    #	doneNDeserted/ps/ListExternalyRegisterdScripts.ps1
    #	doneNDeserted/ps/RegisterRepositoryOfKnownType.ps1
    #	doneNDeserted/ps/out-more.ps1
    #	doneNDeserted/ps/psGraphTest.ps1
    #	doneNDeserted/ps/runCmdAsADmin.ps1
    #	doneNDeserted/ps/show-psobject.ps1
    #	doneNDeserted/ps/unionObject.ps1
    #	importModules.ps1
    #	ps1.code-workspace
    #	random/.gitmodules
    #	random/Microsoft.PowerShellISE_profile.~ps1
    #	random/README.md
    #	random/powerShellProjectFolder.code-workspace
    #	random/ps1.code-workspace
    #	random/test2/Arbetsskikt/ConsoleHistory-6.jso/use what changed to install irfanviewer. then uninstall and just take the file asigning.md
    #	random/test2/Nytt textdokument.json
    #	random/test2/template.config.js
    #	random/treeView.cs
    
    from: How to split CSV file into multiple files using PowerShell
    
    copied answere
    
    [quasicode] split ini and git config
    
    [chore] general usage
    
    moved to submodule
    
    sdad
    
    [chore] submodule update
    
    [chore] Modules as submodule
    
    removing Modules folder
    
    [submodule] updated
    
    [chore] Submodule with Scritps
    
    remove every memmory of Scripts
    
    git remove every memmory+scripts
    
    [Chore] Proper structure
    
    should be in subrepos
    
    merged with old
    
    reverted modules

---
+++ doneNDeserted/git/List-Duplicate-FilesInHead.ps1
@@ -0,0 +1,18 @@
+﻿function list-git-DupeObjectHash
+{
+param([string]$path)
+$current = $PWD
+
+cd $path
+
+git ls-tree -r HEAD |
+    %{ [pscustomobject]@{unkown=$_.substring(0,6);blob=$_.substring(7,4); hash=$_.substring(12,40);relativePath=$_.substring(53)} } |
+        Group-Object -Property hash |
+         ? { $_.count -ne 1 } | 
+            Sort-Object -Property count -Descending |
+                %{ $_.group } 
+ cd $current
+ }               
+                
+ list-git-DupeObjectHash -path 'C:\Users\chris\Desktop\New folder' | 
+                    select @{name="h1";expre={$_.hash.substring(38)}}, relativepath
