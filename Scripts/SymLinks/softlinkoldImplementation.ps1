a4d09ec30d59882b9962c4d70ec1fe96614b0d40
perXautomatik <christoffer.broback@gmail.com>
2021-12-27 16:14:45 +0100

    reverted, past was dupes... maybe the symlink ressults in git deleting the files for real

---
+++ Scripts/SymLinks/softlinkoldImplementation.ps1
@@ -0,0 +1,9 @@
+#if ((Split-Path -Leaf $target) -Ieq (Split-Path -leaf $Source))
+#{
+#$target = Split-Path -Parent $target
+#$source makeSoftLink 'D:\Users\crbk01\AppData\Local\Microsoft\Outlook' 'C:\Users\crbk01\AppData\Local\Microsoft\Outlook'
+#$target
+#$targetName = (Split-Path -Leaf $Source)
+#cd $target
+#(Split-Path -Leaf $Source)
+#}
