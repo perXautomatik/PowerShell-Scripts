da49938d76d3daf1029755773a5890a7d085d1e0
perXautomatik <christoffer.broback@gmail.com>
2022-04-18 01:42:27 +0200

    sdf

--- filesystem/groupInto/GroupIntoFolder.ps1
+++ filesystem/groupInto/GroupIntoFolder.ps1
@@ -1,10 +1,13 @@
 #https://stackoverflow.com/questions/41467996/powershell-create-a-folder-from-a-file-name-then-place-that-file-in-the-folde
 
-Get-ChildItem -File |  # Get files
-  Group-Object { $_.Name -replace '[(]?\d*[)]?$' } |  # Group by part before first underscore
-  ForEach-Object {
+$path = 'C:\Users\crbk01\OneDrive - Region Gotland\Till Github\PowerShell and bash'
+
+ dir $path\*.* | Group-Object -Property extension -AsHashTable | ForEach-Object {
     # Create directory
-    $dir = New-Item -Type Directory -Name $_.Name
-    # Move files there
-    $_.Group | Move-Item -Destination $dir
+    $dir = New-Item -Type Directory -Name $_.BaseName
+    # Move files there  
+    Move-Item -path $_ -Destination $dir 
   }
+
+   # Get files
+ # Group-Object {$_.fullname} |  # Group by part before first underscore
