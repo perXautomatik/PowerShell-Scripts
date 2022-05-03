0beb0a5fe420702c901168608a28d2ca4acd97f6
perXautomatik <christoffer.broback@gmail.com>
2022-05-03 16:09:44 +0200

    [chore] Renamed, restructured
    [chore] Reorganized, deduped,
    
    [chore] renamed
    dsd
    auto
    adasd
    sdf
    initial

---
+++ doneAndDeserted/Git/Submodule-AddexsistingasAndAbsorbeEach.ps1
@@ -0,0 +1,14 @@
+﻿$path = 'D:\Users\crbk01\AppData\Roaming\JetBrains\DataGrip2021.1\consoles\db'
+
+cd $path
+
+Get-ChildItem -Directory | % {
+ 
+ #git submodule add -b "$path\.git" $_.Name
+ $q = $_.fullname+'\.git'
+ git submodule add -b -- $q $_.Name
+ 
+ $_.Name
+ git submodule absorbgitdirs 
+ 
+}
---
+++ doneAndDeserted/filesystem/mergeTwoFilesSmallIntoLargeDeletesmall.ps1
@@ -0,0 +1,28 @@
+﻿#..\ForumAnsweres\pipe-objects-into-arrays.ps1
+
+. "D:\Project Shelf\PowerShellProjectFolder\scripts\TodoProjects\ForumAnsweres\pipe-objects-into-arrays.ps1"
+
+
+function merge-files {
+param([string[]]$a)
+
+#one liner = Get-Content inputFile1.txt, inputFile2.txt | Set-Content joinedFile.txt
+
+ 
+    (Get-Content ($a | Sort-Object -Property @{expres={(Get-Content $_ | Measure-Object).count }} -Descending | Tee-Object -Variable v | chunk-object)) | Set-Content ($v | select -First 1)
+ 
+ trap {
+    throw $_.Exception
+    break
+ }
+
+ ($v | select -Skip 1) | Remove-Item
+
+
+ echo "merged " + (($v | select -Skip 1) | chunk-object )
+ echo "to " + ($v | select -first 1)
+
+
+}
+
+merge-files -a "D:\Project Shelf\PowerShellProjectFolder\scripts\TodoProjects\hello.ps1","D:\Project Shelf\PowerShellProjectFolder\scripts\TodoProjects\hello2.ps1"
