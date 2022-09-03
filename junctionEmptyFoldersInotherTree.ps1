07b812b9b77278ef814cf88e12e7d1885efc3985
Adam the 32-bit Aardvark <codetwo@com/admins-blog/how-to-split-csv-file-into-multiple-files-using-powershell>
2022-09-03 12:10:47 +0200

    from: How to split CSV file into multiple files using PowerShell

---
+++ junctionEmptyFoldersInotherTree.ps1
@@ -0,0 +1,3 @@
+﻿cd 'B:\Users\chris\Documents'
+
+$emptyDirectories = (Get-ChildItem -Directory | ? { ($_ | Get-ChildItem).length -eq 0 }); $relatedDirectories = @{} ; Get-ChildItem '.\My Games\' -Directory | ? { $emptyDirectories.name -contains $_.name } | %{ $relatedDirectories.add($_.name,$_)} ; $emptyDirectories | %{rm $_ ; junction.exe $_ $relatedDirectories[$_.Name].FullName  }
--- splitIni.ps1
+++ splitIni.ps1
@@ -34,6 +34,7 @@
 
     }
     $stream.Close()
+}
 
 
 $from = "C:\temp\large_log.txt"
@@ -65,4 +66,27 @@ try {
 finally {
     $fromFile.Close()
 }
+
+# variable used to store the path of the source CSV file
+$sourceCSV = <path of source CSV> ;
+
+# variable used to advance the number of the row from which the export starts
+$startrow = 0 ;
+
+# counter used in names of resulting CSV files
+$counter = 1 ;
+
+# setting the while loop to continue as long as the value of the $startrow variable is smaller than the number of rows in your source CSV file
+while ($startrow -lt <total number of rows in source CSV>)
+{
+
+# import of however many rows you want the resulting CSV to contain starting from the $startrow position and export of the imported content to a new file
+Import-CSV $sourceCSV | select-object -skip $startrow -first <number of rows in resulting CSV> | Export-CSV "<resulting CSV filepath>$($counter).csv" -NoClobber;
+
+# advancing the number of the row from which the export starts
+$startrow += <number of rows in resulting CSV> ;
+
+# incrementing the $counter variable
+$counter++ ;
+
 }
