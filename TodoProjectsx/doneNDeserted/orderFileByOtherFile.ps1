2e2ff34ed5a0967b436d6eb81060b2250c12f8cf
perXautomatik <christoffer.broback@gmail.com>
2022-09-09 22:59:27 +0200

    [Feature] Order File a by File b

---
+++ TodoProjectsx/doneNDeserted/orderFileByOtherFile.ps1
@@ -0,0 +1,22 @@
+﻿
+
+#sort list by order of other list
+
+function Sort-byOrderOf () {
+
+
+}
+
+$pathToBeOrdered = 
+'C:\Users\chris\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt'
+$refencePath = 
+'C:\Users\chris\AppData\Local\Temp\TortoiseGit\ConsoleHost_history-613cc802.000.txt'
+
+$byteArray = Get-Content -Path $refencePath -Raw
+
+#Get-Member -InputObject $bytearray
+
+
+Get-Content $pathToBeOrdered | Sort-Object -Property @{expr={$byteArray.IndexOf($_)}; desc=$false} | Set-Content $pathToBeOrdered
+
+
