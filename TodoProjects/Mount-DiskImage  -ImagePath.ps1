eea82cb4a0d7559d6f8d4ade2844f919bed57193
perXautomatik <christoffer.broback@gmail.com>
2022-09-09 23:14:55 +0200

    [Feature] Broken out from Scrips module
    # Conflicts:
    #	TodoProjects/random/.gitmodules

---
+++ TodoProjects/Mount-DiskImage  -ImagePath.ps1
@@ -0,0 +1,19 @@
+﻿<#
+$virtualDrivePath = 'U:\VirtualDrive\'
+
+| % {
+
+switch($_.basename)
+'BiggerSteamLib' {}
+
+}
+
+
+Mount-DiskImage  -ImagePath "\\100.84.7.151\NetBackup\VirtualDrive\BiggerSteamLib.vhdx" -NoDriveLetter -Passthru | 
+    Get-Disk | 
+    Get-Partition | 
+    where { ($_ | Get-Volume) -ne $Null } | 
+    Add-PartitionAccessPath -AccessPath "D:\SteamV\steamapps\common"
+
+#>
+Get-ChildItem '\\100.84.7.151\NetBackup\VirtualDrive\*' -include *.vhdx | select fullname
