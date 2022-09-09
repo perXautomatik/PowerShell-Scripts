345bcfad4574a2e51b33fcacee08610d952c8e30
perXautomatik <christoffer.broback@gmail.com>
2022-09-09 22:54:56 +0200

    [feature] list Externaly Registered Scripts

---
+++ TodoProjectsx/doneNDeserted/ListExternalyRegisterdScripts.ps1
@@ -0,0 +1,5 @@
+﻿Get-Command -CommandType ExternalScript |Select-Object -property Name,Path,
+@{Name="LastWriteTime";Expression={(Get-Item $_.path).lastWriteTime}},
+@{Name="Location";Expression={Split-Path $_.source}} | 
+Sort-Object -Property Location |
+Format-Table -GroupBy Location -property Name,LastWriteTime
