7a43ff294899a804ad7eaab1a235c01ac10171d5
perXautomatik <christoffer.broback@gmail.com>
2022-09-10 02:56:50 +0200

    [chore] Deleted Dupes and unessesary

--- TodoProjectsx/doneNDeserted/ListExternalyRegisterdScripts.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-Command -CommandType ExternalScript |Select-Object -property Name,Path,
-@{Name="LastWriteTime";Expression={(Get-Item $_.path).lastWriteTime}},
-@{Name="Location";Expression={Split-Path $_.source}} | 
-Sort-Object -Property Location |
-Format-Table -GroupBy Location -property Name,LastWriteTime
