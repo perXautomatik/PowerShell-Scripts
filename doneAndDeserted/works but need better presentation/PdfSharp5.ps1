01c0b9faef3ca83e79200783640ed73ff315f849
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 11:14:37 +0200

    moved out of pshell

--- doneAndDeserted/works but need better presentation/PdfSharp5.ps1
+++ doneAndDeserted/works but need better presentation/PdfSharp5.ps1
@@ -1,18 +1,3 @@
-﻿;use what changed to install irfanviewer. then uninstall and just take the file asigning.
-;install everything
-;bcompare.
-;steam.
-
-;all images linked to one hdd
-
-;all sounds ans music to one hdd
-
-;do so for all extensions, question being, would it be extreamly heavy for the storage?
-;we copy the full path to the new drive to 
-
-;it would be easier to overview batchprocesses, easier to find duplicates and possible to identify filesm whoere actually the same but due to some minor detale consideed to be different.
-
-;wonder if one could guess file extension while processing the files, like files without filename, the link can be renamed with actual filename after all.
 Imports PdfSharp.Drawing
 Imports PdfSharp.Pdf
 Imports System.Text.RegularExpressions
@@ -35,7 +20,7 @@ write-host "$($drive2) has  $($diskdata.Used) Used and $($diskdata.Free) free"
 else
 {return} # don't bother testing for drive3 since we didn't even have 
 
-    function MyFunction(ByVal cmdArgs As String)
+    'function MyFunction(ByVal cmdArgs As String)
 		@document = PdfSharp.Pdf.IO.PdfReader.Open(cmdArgs, PdfSharp.Pdf.IO.PdfDocumentOpenMode.Import)
         Console.WriteLine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData))
         @rgx Regex("^[0-9]{16}$")
@@ -63,4 +48,4 @@ else
         Next
 	
         End function
-
+        '
