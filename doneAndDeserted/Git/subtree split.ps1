a153f05839741345cbb31337393bdf0100042cf3
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 11:36:17 +0200

    Improved readability

--- doneAndDeserted/Git/subtree split.ps1
+++ doneAndDeserted/Git/subtree split.ps1
@@ -1,3 +1,4 @@
 ﻿cd D:\Users\crbk01\AppData\Roaming\JetBrains\Datagrip\consolex\db\8cc23256-e02c-43ea-8477-e740c4580b62
-
-git subtree split --prefix=Organized/SolVision -b SolVision
+$newBranchName = 'SolVision'
+$relativePath = 'Organized/SolVision'
+git subtree split --prefix=$relativePath -b $newBranchName
