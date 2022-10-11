017fd87eadb6c1d49a46efcda8c30fd2b385aaba
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 23:24:25 +0200

    modifications

--- random/Microsoft.PowerShellISE_profile.ps1
+++ random/Microsoft.PowerShellISE_profile.ps1
@@ -2,7 +2,11 @@
 
 #Module Browser Begin
 #Version: 1.0.0
+<<<<<<< HEAD
+Add-Type -Path 'E:\Program Files (x86)\Microsoft Module Browser\ModuleBrowser.dll'
+=======
 Add-Type -Path 'C:\Program Files (x86)\Microsoft Module Browser\ModuleBrowser.dll'
+>>>>>>> 6e627396db00db8e49ff224ee404bee634c268fb
 $moduleBrowser = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Module Browser', [ModuleBrowser.Views.MainView], $true)
 $psISE.CurrentPowerShellTab.VisibleVerticalAddOnTools.SelectedAddOnTool = $moduleBrowser
 #Module Browser End
