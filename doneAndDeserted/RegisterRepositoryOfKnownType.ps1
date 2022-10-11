6410fe2d07dffc3014526a476fe6cbac878eaea0
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 11:11:32 +0200

    [feature] Technically done but i need to reasure my self it's done
    moved out of pshell

---
+++ doneAndDeserted/RegisterRepositoryOfKnownType.ps1
@@ -0,0 +1,8 @@
+﻿$parameters = @{
+  Name = "Sfta"
+  ScriptSourceLocation  = "https://raw.githubusercontent.com/DanysysTeam/PS-SFTA/master/SFTA.ps1"
+  PublishLocation = "https://github.com/DanysysTeam/PS-SFTA"
+  InstallationPolicy = 'Trusted'
+}
+Register-PSRepository @parameters
+Get-PSRepository
