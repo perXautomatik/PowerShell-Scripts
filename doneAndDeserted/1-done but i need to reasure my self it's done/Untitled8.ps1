bbf7c9eb376ec6220c82a6fdd3d01078153c2d63
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 11:50:54 +0200

    Moved arround and Reintroduced from pshell
    Moved arround
    
    moved around
    
    Reorganized
    
    Alternative version
    
    Moved around
    
    Alternative version
    
    From Pshell

--- doneAndDeserted/1-done but i need to reasure my self it's done/Untitled8.ps1
+++ doneAndDeserted/1-done but i need to reasure my self it's done/Untitled8.ps1
@@ -1,4 +1,3 @@
 ﻿cd D:\Users\crbk01\AppData\Roaming\JetBrains\Datagrip\consolex\db\8cc23256-e02c-43ea-8477-e740c4580b62
-   
-git ls-files | git grep -v "^SolVision" 
 
+git subtree split --prefix=Organized/SolVision -b SolVision
