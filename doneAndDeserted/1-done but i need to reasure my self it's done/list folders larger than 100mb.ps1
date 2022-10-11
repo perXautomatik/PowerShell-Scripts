f5e49417367c874baf065b237db7158e48718969
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 12:01:10 +0200

    Moved arround

--- doneAndDeserted/1-done but i need to reasure my self it's done/list folders larger than 100mb.ps1
+++ doneAndDeserted/1-done but i need to reasure my self it's done/list folders larger than 100mb.ps1
@@ -1,21 +1,7 @@
 ﻿Import-Module SplitPipeline
 
+. .recuse
 
-
-function Recurse($path) {
-
-  $fc = new-object -com scripting.filesystemobject
-  $folder = $fc.getfolder($path)
-
-  foreach ($i in $folder.files) { $i | select Path }
-
-  foreach ($i in $folder.subfolders) {
-    $i | select Path        
-    if ( (get-item $i.path).Attributes.ToString().Contains("ReparsePoint") -eq $false) {        
-        Recurse($i.path)
-    }
-  }
-}
 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $outputlist = Recurse($scriptPath)
