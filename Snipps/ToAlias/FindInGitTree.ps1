382f92f9e05a2dff2528ef60e9944890ecd952f9
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 11:50:54 +0200

    Moved arround and Reintroduced from pshell
    Moved arround
    
    moved around
    
    Reorganized
    
    Alternative version
    
    Moved around
    
    Alternative version
    
    From Pshell

---
+++ Snipps/ToAlias/FindInGitTree.ps1
@@ -0,0 +1,4 @@
+﻿cd D:\Users\crbk01\AppData\Roaming\JetBrains\Datagrip\consolex\db\8cc23256-e02c-43ea-8477-e740c4580b62
+   
+git ls-files | git grep -v "^SolVision" 
+
--- doneAndDeserted/chocoInstall.ps1
+++
@@ -1,2 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
-
