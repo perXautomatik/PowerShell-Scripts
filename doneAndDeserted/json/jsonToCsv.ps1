808574c79f91b8b432541ff7a9abcb22b2a5068f
perXautomatik <christoffer.broback@gmail.com>
2022-10-11 12:03:51 +0200

    moved around

--- doneAndDeserted/json/jsonToCsv.ps1
+++ doneAndDeserted/json/jsonToCsv.ps1
@@ -17,11 +17,7 @@ foreach ($p in $hash.GetEnumerator())
 };$ressult
 }}
 
-Function Union-Object ([String[]]$Property = @()) {             # https://powersnippets.com/union-object/
-    $Objects = $Input | ForEach {$_}                            # Version 02.00.01, by iRon
-    If (!$Property) {ForEach ($Object in $Objects) {$Property += $Object.PSObject.Properties | Select -Expand Name}}
-    $Objects | Select ([String[]]($Property | Select -Unique))
-} Set-Alias Union Union-Object
+. ./unionObject
 
 $ErrorActionPreference="SilentlyContinue"
 Stop-Transcript | out-null
