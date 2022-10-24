99f1d1f28dd164aa09dab35a6d7412b922b4755f
perXautomatik <christoffer.broback@gmail.com>
2022-10-24 14:08:37 +0200

    Feature Todo files

---
+++ TodoProjects/git-TreeFilterRemeveEverythingExcept.ps1
@@ -0,0 +1,3 @@
+﻿cd 'B:\chris\Documents\New folder (3)\Beyond Compare 4'
+
+git filter-branch --index-filter 'git rm --cached -qr --ignore-unmatch -- . && git reset -q $GIT_COMMIT -- .gitignore' --prune-empty -- --all
