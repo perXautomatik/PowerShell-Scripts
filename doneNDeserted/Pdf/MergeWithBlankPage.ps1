2ce4f3e25bfc66746e693f6efe5236b875bdffac
Christoffer Brobäck <christoffer.broback@gmail.com>
2023-07-05 18:54:05 +0200

    +\random\Pdf\MergeWithBlankPage.ps1 -> \doneNDeserted\Pdf\MergeWithBlankPage.ps1+

--- doneNDeserted/Pdf/MergeWithBlankPage.ps1
+++ doneNDeserted/Pdf/MergeWithBlankPage.ps1
@@ -1,17 +1,10 @@
 ﻿         
-<<<<<<< HEAD
-    $emptyPage = "G:\sbf\Livsmiljö\Gemensamt\mapbasic program projekt\MapBasic-Gis_OrganizeEmails\tomtPdfBlad.pdf"
-    $path = "C:\Users\crbk01\Desktop\mellersta"
-    $i = 1
-    $param1 = "C:\Users\crbk01\OneDrive - Region Gotland\PortableApps\cpdf\cpdf.exe"
-=======
     $emptyPage = "G:\sbf\Livsmiljö\Gemensamt\mapbasic program projekt\organiserarFörMailboxen\tomtPdfBlad.pdf"
     $path = "C:\Users\crbk01\Desktop\norra"
     $i = 1
     $param1 = "C:\Users\crbk01\OneDrive - Region Gotland\PortableApps\cpdf\cpdf" 
->>>>>>> a471816 (dasådasd)
     $param2 = "-pages"
-
+ function merge-with-blank-page ($emptyPage,$path,$i,$param1,$param2 )
     dir $path\*.pdf | Sort-Object -Property CreationTime -Descending | foreach-object{   
        
 
