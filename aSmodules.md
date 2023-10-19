3e1a6683628fe74b4e2c42d5f95d2f2f3a0a3af8
Christoffer Brobäck <christoffer.broback@gmail.com>
2023-10-19 13:40:04 +0200

    state

---
+++ aSmodules.md
@@ -0,0 +1,3 @@
+for each folder Name
+apped to gitignore if not already there
+add to submodule if not already there
---
+++ jsonBrowser.ps1
@@ -0,0 +1,33 @@
+﻿
+function Resolve-Properties
+{
+    param ([parameter(ValueFromPipeline)][object]$inputObject)
+
+    process {
+        foreach($prop in $inputObject.psobject.Properties)
+        {
+        
+        if  ( $prop.value.count -gt 1 )      { Resolve-Properties $prop.Value }        
+        else
+        {
+            [pscustomObject]@{
+                Name = $prop.Name  
+                value = $prop.Value
+            }
+        }
+        
+        }
+    }
+}
+
+cd 'C:\Users\chris\Documents\394c4bae9f3023729a6ccb4aa1fff31d'
+
+(Get-Content .\turk | ConvertFrom-Json ) | 
+% { $_.psobject.Properties} | 
+    ?{ $_.value.count -gt 1 } | 
+        % { $_.psobject.Properties.value} 
+        
+        
+        
+        
+          # | Resolve-Properties
